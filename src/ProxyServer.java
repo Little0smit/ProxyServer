68f3c6cf6f85892168bf211a5f5252d55c1c7cc7
Duncan Smith <dunc_97@hotmail.co.uk>
2016-11-18 12:52:57 +0000

    Basic Proxy working. TODO: get those bitches multi-threading         Different error codes         That extension shit

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -6,20 +6,17 @@ import java.net.ServerSocket;
 public class ProxyServer {
 
     public static void main(String[] args){
+        boolean listening = true;
         try {
-            if (args.length != 3){
-                System.out.println("Insufficient arguments");
-            }
-            String hostURL = args[0];
-            int localPort = Integer.parseInt(args[1]);
-            int remotePort = Integer.parseInt(args[2]);
-            System.out.println("Starting proxy server for " + hostURL + ":" + remotePort + " on port: " +  localPort);
+            int port = Integer.parseInt(args[0]);
 
             // Start the server
-            ServerSocket proxyServer = new ServerSocket(localPort);
+            ServerSocket proxyServer = new ServerSocket(port);
+            System.out.println("Started on port: " + port);
             do {
-                new ProxyThread(proxyServer.accept());
-            } while(true);
+                new ProxyThread(proxyServer.accept()).start();
+            } while(listening);
+            proxyServer.close();
         } catch (IOException err){
 
         }
--- src/ProxyThread.java
+++ src/ProxyThread.java
@@ -1,16 +1,18 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.StringTokenizer;
 
 /**
  */
 public class ProxyThread extends Thread {
     private Socket serverClient;
+    private static final int BUFFER_SIZE = 32768;
     ProxyThread(Socket serverClient){
+        super("ProxyThread");
         this.serverClient = serverClient;
-        this.start();
     }
 
     @Override
@@ -39,8 +41,70 @@ public class ProxyThread extends Thread {
                 }
                 count++;
             }
-        } catch (Exception e){
+            /////////////////////////////////
 
+            BufferedReader reader = null;
+            try {
+                //begin send request to server, get response from server
+                URL url = new URL(urlToCall);
+                HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+                httpConnection.setDoInput(true);
+                httpConnection.setDoOutput(false);
+
+                //Get the response]
+                InputStream inStream = null;
+                try {
+                    switch (httpConnection.getResponseCode()) {
+                        case 200:
+                            inStream = httpConnection.getInputStream();
+                            reader = new BufferedReader(new InputStreamReader(inStream));
+                            break;
+                        case 404:
+                            System.out.println("404 Page not found");
+
+                        default:
+                            break;
+                    }
+                } catch (IOException err) {
+                    System.err.println("IO EXCEPTION!!: " + err);
+                }
+
+                System.out.println();
+
+                //Begin send response to client
+                byte[] by = new byte[BUFFER_SIZE];
+                assert inStream != null;
+                try {
+                    int index = inStream.read(by, 0, BUFFER_SIZE);
+                    while (index != -1) {
+                        out.write(by, 0, index);
+                        index = inStream.read(by, 0, BUFFER_SIZE);
+                    }
+                } catch (NullPointerException e) {
+
+                }
+                out.flush();
+
+            } catch (Exception e) {
+                System.err.println("Encountered exception: " + e);
+                e.printStackTrace();
+                out.writeBytes("");
+            }
+            //close out all resources
+            if (reader != null) {
+                reader.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (in != null) {
+                in.close();
+            }
+            if (serverClient != null) {
+                serverClient.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
         };
     }
 }
